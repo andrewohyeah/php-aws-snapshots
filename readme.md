f5ef87ce77826eb58013fdacdae6a4ce210a74a3
Andrew <andrew@sitesol.ca>
2016-11-12 14:01:46 -0800

    changed to delete so to keep latest n snapshots

--- readme.md
+++ readme.md
@@ -1,6 +1,4 @@
-Create automatic AWS EC2 snapshots with limits on the total number of snapshots created and the interval which the snapshots are created.
-
-For example, you could create a snapshot every day and only keep the last 7 for a running week's worth of snapshots. Or create a snapshot once a week and only keep the last 4 so you would have a running month's worth of snapshots.
+Delete AWS snapshots, keeping only the latest number of snapshots specified
 
 
 ## Requirements
@@ -17,10 +15,9 @@ This assumes you've already installed and setup [AWS CLI](http://aws.amazon.com/
 <?php
 require_once('snapshots.php');
 $volumes = array(
-   'vol-123af85a' => array('snapshots' => 7, 'interval' => '1 day', 'description' => 'dev server backup'),
-   'vol-321bg96c' => array('snapshots' => 4, 'interval' => '1 week', 'description' => 'image server'),
+   'vol-123af85a' => array('keep' => 7)
 );
-$snapshots = new snapshots($volumes);
+$snapshots = new DeleteSnapshots($volumes);
 $snapshots->run();
 ```
 ### 2. Add cron job
@@ -35,23 +32,8 @@ The cron job schedule will depend on your configuration. The class honors the in
 | Name | Type | Description |
 |------|------|-------------|
 | *volume id* | string | AWS EBS volume ID
-| snapshots | integer | total number of snapshots to store for volume |
-| interval | string | how often to create snapshot (1 day, 7 days, 2 weeks - full list below)
-| description | string | snapshot description that shows in the Snapshot section within AWS console |
+| keep | integer | total number of snapshots to store for volume |
 
-### Interval Values
-The format **must** be `number type`
-
-Valid types:
-- hour, hours
-- day, days
-- week, weeks
-- month, months
-- year, years
-
-Not practical but you *could* also us
-- second, seconds
-- minute, minutes
 
 ## Example IAM Policy
 This is a minimal policy that includes ONLY the permissions needed to work. You could also limit the "Resources" option to restrict it even further.
@@ -62,7 +44,6 @@ This is a minimal policy that includes ONLY the permissions needed to work. You 
     {
       "Effect": "Allow",
       "Action": [
-        "ec2:CreateSnapshot",
         "ec2:DeleteSnapshot",
         "ec2:DescribeSnapshots"
       ],
@@ -82,6 +63,3 @@ sudo pip install awscli
 // must set region - ie: us-east-1, us-west-1
 aws configure
 ```
-
-## Questions, issues or suggestions
-Please use the [issues section](https://github.com/jveldboom/php-aws-snapshots/issues) for any questions or issues you have. Also, suggestions, pull request or any help is most welcome!
--- snapshots.php
+++ snapshots.php
@@ -1,69 +1,31 @@
 <?php
-class snapshots
+class DeleteSnapshots
 {
-	function __construct($volumes)
+	protected $volumes = null;
+
+	protected $options = null;
+
+	public function __construct($volumes)
 	{
 		$this->volumes = $volumes;
 	}
 	
-	function run()
+	public function run()
 	{
 		foreach($this->volumes as $volume_id => $options)
 		{
-			if(!self::setOptions($options)){
+			if(!$this->setOptions($options)){
 				echo 'Volume '.$volume_id.' not ran due to invalid config options'.PHP_EOL;
 				continue;
 			};
-			
-			$snapshots = self::getSnapshots(array('volume-id'=>$volume_id,'description'=>$this->options['description']));
-			if(!$snapshots) continue;
-			
-			if(self::shouldCreate($snapshots)){
-				self::create($volume_id,$this->options['description']);
-			}
-			
+						
 			// delete extra snapshots base on number of 'snapshots' option
-			self::deleteExtra($volume_id);
+			$this->deleteExtra($volume_id);
 		}
 
 		return true;
 	}
 	
-	/**
-	 * Check if a snapshot should be created based set options (number of snapshots & interval)
-	 * @param  object $snapshots list of snapshots return from AWS CLI
-	 * @return boolean
-	 */
-	private function shouldCreate($snapshots)
-	{
-		// create snapshot if none exist and the option is set for 1 or more
-		if(count($snapshots->Snapshots) < 1 && $this->options['snapshots'] > 0) return true;
-		
-		$interval = (new \DateTime())->modify('-'.$this->options['interval']);
-		$last_snapshot = new DateTime(end($snapshots->Snapshots)->StartTime);
-
-		// use same timezones for comparison below
-		$interval->setTimezone(new DateTimeZone('EDT'));
-		$last_snapshot->setTimezone(new DateTimeZone('EDT'));
-
-		// check if last snapshot is before the interval time-frame
-		if($last_snapshot < $interval) return true;
-		
-		return false;
-	}
-
-	/**
-	 * Create new EBS snapshot
-	 * @param  string $volume_id
-	 * @param  string $description
-	 * @return string
-	 */
-	public function create($volume_id,$description='PHP Snapshots')
-	{
-		$cmd = sprintf('/usr/local/bin/aws ec2 create-snapshot --volume-id %s --description "'.$description.'"',escapeshellarg($volume_id));
-		return shell_exec($cmd);
-	}
-
 	/**
 	 * Delete snapshot
 	 * @param  string $snapshot_id
@@ -82,15 +44,18 @@ class snapshots
 	 */
 	private function deleteExtra($volume_id)
 	{
-		$snapshots = self::getSnapshots(array('volume-id'=>$volume_id,'description'=>$this->options['description']));
-		$snapshot_count = count($snapshots->Snapshots);
-		
-		if($snapshot_count <= $this->options['snapshots']) return false;
-		
-		for($x=0;$x<$snapshot_count - $this->options['snapshots']; ++$x)
-		{
-			self::delete($snapshots->Snapshots[$x]->SnapshotId);
-		}		
+		$snapshots = $this->getSnapshots(array('volume-id'=>$volume_id));
+
+		if( $snapshots !== false ){
+			$snapshot_count = count($snapshots->Snapshots);
+			
+			if($snapshot_count <= $this->options['snapshots']) return false;
+			
+			for($x=0;$x<$snapshot_count - $this->options['snapshots']; ++$x)
+			{
+				$this->delete($snapshots->Snapshots[$x]->SnapshotId);
+			}			
+		}
 	}
 	
 	/**
@@ -101,7 +66,9 @@ class snapshots
 	public function getSnapshots($filters=array())
 	{
 		$cmd_filters = false;
-		foreach($filters as $name => $value) $cmd_filters .= 'Name='.escapeshellarg($name).',Values='.escapeshellarg($value).' ';
+		foreach($filters as $name => $value) {
+			$cmd_filters .= 'Name='.escapeshellarg($name).',Values='.escapeshellarg($value).' ';
+		}
 
 		$cmd = '/usr/local/bin/aws ec2 describe-snapshots '.($cmd_filters ? '--filters '.trim($cmd_filters) : '');
 		$response = shell_exec($cmd);
@@ -124,12 +91,10 @@ class snapshots
 	 */
 	private function setOptions($options)
 	{
-		if(!isset($options['snapshots']) || !isset($options['interval'])) return false;
+		if(!isset($options['keep']) ) return false;
 
 		$this->options = array(
-			'snapshots'   => (int) $options['snapshots'],
-			'interval'    => $options['interval'],
-			'description' => $options['description'],
+			'snapshots'   => (int) $options['keep']
 		);
 		
 		return true;
